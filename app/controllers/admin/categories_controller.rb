b9c6031ffdfb262368b8ac70971f658cd5ad3a39
William GT <willimail|@gmail.com>
2016-01-29 22:00:54 +0000

    fix error id

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,7 +25,14 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    
+    id = params[:id]
+    if id == nil
+      id = Category.first.id
+    end
+    @category = Category.find(id)
+    
+    # @category = Category.find(params[:id])
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
--- features/create_category.feature
+++ features/create_category.feature
@@ -9,12 +9,3 @@ Feature: Login as a Admin
     And I fill in "password" with "pkTBQbh"
     Then I should be on the admin content page
     
-Feature: Add a movie to Rotten Potatoes
-  As a movie fan 
-  So that I can share a movie with another movie fans
-  I want to add a movie to Rotten Potatoes database
-Feature: name
-  As a [Kind of stackeholder]
-  So that [I can achieve some goal]
-  I want to do [tasks]
-    
